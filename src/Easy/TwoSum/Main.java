380b9aaa38cf949b855db0aa86dc94bdc6367426
PiotrAndrzejewsky <piotrandrzejewski@onet.eu>
2022-12-21 22:23:46 +0100

    feat: initial files and twoSums

---
+++ src/Easy/TwoSum/Main.java
@@ -0,0 +1,25 @@
+package Easy.TwoSum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        twoSum(new int[]{2, 7, 11, 15}, 9);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int result;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{0};
+    }
+}
